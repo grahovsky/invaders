88d0182e9315969a18987d74bb486874e782eb25
grahovsky <grahovsky@gmail.com>
2022-08-05 19:45:26 +0300

    add game engine

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use std::error::Error;
+use std::io;
+use std::time::Duration;
+use crossterm::{event, ExecutableCommand, terminal};
+use crossterm::cursor::{Hide, Show};
+use crossterm::event::{Event, KeyCode};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use rusty_audio::Audio;
 
 fn main() -> Result <(), Box<dyn Error>> {
+
     let mut audio = Audio::new();
     audio.add("explode", "sounds/explode.wav" );
     audio.add("lose", "sounds/lose.wav" );
@@ -9,9 +16,36 @@ fn main() -> Result <(), Box<dyn Error>> {
     audio.add("pew", "sounds/pew.wav" );
     audio.add("startup", "sounds/startup.wav" );
     audio.add("win", "sounds/win.wav" );
-    audio.play("explode");
+    audio.play("startup");
+
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Game Loop
+
+    'gameloop: loop {
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+    };
 
     //Cleanup
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
+
 }
