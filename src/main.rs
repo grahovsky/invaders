f21eed3ff76edcc2c94797eb3ab95a23325e1dc2
grahovsky <grahovsky@gmail.com>
2022-08-11 22:35:02 +0300

    add invaders

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use rusty_audio::Audio;
 
 use invaders::{frame, render};
 use invaders::frame::{Drawable, new_frame};
+use invaders::invaders::Invaders;
 use invaders::player::Player;
 
 
@@ -50,6 +51,7 @@ fn main() -> Result <(), Box<dyn Error>> {
     // Game Loop
     let mut player = Player::new();
     let mut instant = Instant::now();
+    let mut invaders = Invaders::new();
     'gameloop: loop {
         // Per-frame  init
         let delta = instant.elapsed();
@@ -78,9 +80,19 @@ fn main() -> Result <(), Box<dyn Error>> {
 
         // Updates
         player.update(delta);
+        if invaders.update(delta) {
+            audio.play("move");
+        }
 
         // Draw & render
-        player.draw(&mut curr_frame);
+
+        // player.draw(&mut curr_frame);
+        // invaders.draw(&mut curr_frame);
+        let drawables: Vec<&dyn Drawable> = vec![&player, &invaders];
+        for drawable in drawables {
+            drawable.draw(&mut curr_frame);
+        }
+
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(17));
     };
